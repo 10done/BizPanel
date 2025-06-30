9eaa109135e86a2cdb2f0cb7afff6d542cfe0a22
10done <anubhavtandon6424@gmail.com>
2025-06-30 17:39:59 +0530

    Basic layout first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route, Routes} from 'react-router-dom';
+import Layout from './Components/Layouts/Layout';
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="dashboard">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path="Dashboard" element={<Dashboard/>}>
+          
+          </Route>
+        </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
